5c082cbdd6ea48f5f487bae4a1880b7367605aa6
Doria da Silveira <kamilla.silveira@cencosud.com.br>
2020-08-06 22:37:26 -0300

    wedw

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,16 +1,71 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import FormField from '../../../components/FormFields';
+
 
 function CadastroCategoria() {
+
+  const [categorias, setCategorias] = useState([]);
+
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  }
+
+  const [values, setValues] = useState(valoresIniciais);
+
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor,
+    })
+  }
+
+  function handleChange(infosDoEvento) {
+    setValue(
+      infosDoEvento.target.getAttribute('name'),
+      infosDoEvento.target.value,
+    );
+  }  
+
     return (
       <PageDefault>
-       <h1>Cadastro de Categoria</h1>
-       <form>
-         <label>
-           Nome da Categoria:  
-           <input type="text"/>
-         </label>
+       <h1>Cadastro de Categoria: </h1>
+       <form onSubmit={function handleSubmit(infosDoEvento){
+         infosDoEvento.preventDefault();
+         setCategorias([
+           ...categorias,
+           values
+         ]);
+         setValues(valoresIniciais)
+         
+       }}>
+         
+         
+         <FormField 
+         label="Nome"
+         type="text"
+         name="nome"
+          value={values.nome}
+          onChange={handleChange}/>
+         
+         
+          <FormField 
+         label="Descrição"
+         type="textarea"
+         name="descricao"
+          value={values.descricao}
+          onChange={handleChange}/>
+         
+         <FormField 
+         label="Cor"
+         type="color"
+         name="cor"
+          value={values.cor}
+          onChange={handleChange}/>
+        
          <p></p>
          <button>
            Cadastrar
@@ -18,6 +73,17 @@ function CadastroCategoria() {
         <p></p>
          <Link to="/"> Voltar</Link>
        </form>
+
+       <u>
+         {categorias.map((categoria, indice) => {
+           return (
+             <li key={`${categoria}${indice}`}>
+               {categoria.nome}
+             </li>
+
+           )
+         })}
+       </u>
 
         
       </PageDefault>
